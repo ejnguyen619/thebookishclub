002b8af6e7ba3e67eef32c45f0c87f54669f5753
Edward <edwardn2147@yahoo.com>
2020-11-28 15:21:10 -0800

    Backend: Retrieve user info with ID

--- src/components/SignOffButton.js
+++ src/components/SignOffButton.js
@@ -3,7 +3,12 @@ import './Button.css';
 import { Link } from 'react-router-dom';
 
 export function SignOffButton({setLoggedIn}) {
-  const Logoff = () => setLoggedIn(false);
+  const Logoff = () => {
+    if(localStorage.getItem('userName') !== null) localStorage.removeItem("userName");
+    if(localStorage.getItem('userEmail') !== null) localStorage.removeItem("userEmail");
+    if(localStorage.getItem('userId') !== null) localStorage.removeItem("userId");
+    setLoggedIn(false);
+  }
   return (
     <Link to='/'>
       <button className='btn' onClick={Logoff}>
